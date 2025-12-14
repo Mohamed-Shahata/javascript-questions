c1bd6598348ba8948c92c5b670713605b7b4679c
Mohamed-Shahata <mohamedmrslan@gmail.com>
2025-12-14 05:53:00 -0200

    add new files

--- 21.js
+++ 21.js
@@ -1,2 +1,8 @@
 const obj = { a: "one", b: "two", a: "three" };
 console.log(obj);
+
+/**
+ * 1: { a: "one", b: "two" }
+ * 2: { b: "two", a: "three" }
+ * 3: { a: "three", b: "two" }
+ */
---
+++ 22.js
@@ -0,0 +1,8 @@
+console.log(typeof typeof 1); //
+
+/**
+ * 1 - "number"
+ * 2 - "string"
+ * 3 - "object"
+ * 4 - "undefined"
+ */
